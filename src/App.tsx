6bc5e05ecfec1a0d5b4836473d44f030e3cc6ec7
benedictusyoga <yogatama4561@gmail.com>
2026-01-26 20:39:03 +0700

    fix: removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {

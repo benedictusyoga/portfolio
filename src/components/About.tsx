27ff1253d3f30f485d8805d46c748959b61995df
benedictusyoga <yogatama4561@gmail.com>
2026-01-27 21:15:19 +0700

    fix: spacing in about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,11 +12,13 @@ const About: React.FC = () => {
                     <div className="image-fade-overlay"></div>
                 </div>
                 <div className="about-content">
-                    <h2 className="about-greeting">HI, I'M</h2>
-                    <h1 className="about-name">benedict</h1>
+                    <div className="about-title-group">
+                        <h2 className="about-greeting">HI, I'M</h2>
+                        <h1 className="about-name">benedict</h1>
+                    </div>
                     <p className="about-description">
-                        AS A <strong>PROGRAMMER</strong>, I COMBINE INGENUITY<br />
-                        AND CREATIVITY TO DISSOLVE BOUNDARIES
+                        AS A <strong><i>PROGRAMMER</i></strong>, I COMBINE <i>INGENUITY</i><br />
+                        AND <i>CREATIVITY</i>{" "}TO DISSOLVE <i>BOUNDARIES</i>
                     </p>
                     <button className="read-more-btn">
                         Read More About Me <IoMdArrowRoundForward />
